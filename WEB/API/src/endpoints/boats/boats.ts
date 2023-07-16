71354e4167b1a599d6f52cadcc103bc6a365ba60
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-17 01:19:07 +0300

    boats API

---
+++ WEB/API/src/endpoints/boats/boats.ts
@@ -0,0 +1,16 @@
+import Queries from '../../queries/queries'
+
+export default class Boats {
+  database: Queries
+
+  constructor(database: Queries) {
+    this.database = database
+  }
+
+  // getOne
+  // getAll
+
+  // create
+  // delete
+  // update
+}
