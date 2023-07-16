0229b8e589bfbdeae5226c5c037af91b1cc87665
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-17 01:21:10 +0300

    base

--- WEB/API/src/index.ts
+++ WEB/API/src/index.ts
@@ -1,56 +1,50 @@
 import express = require('express')
 //import bcrypt = require('bcrypt')
-//import Database = require('./data/queries')
+import Queries from './queries/queries'
+import Endpoints from './endpoints/endpoints'
 //import dotenv => To read vars from ENV
-/*interface Database {
-  Database: typeof Database
-}*/
 
-import Test from './test'
-const tst = new Test('asd')
-tst.print()
-
-const app = express()
-const port = 8000
-
-/*const db = new Database() /*{
+// TODO: How to handle secrets/configs?
+// Use ENV? https://node-postgres.com/apis/client
+const postgresConfig = {
   user: 'username',
   password: 'password',
   host: 'localhost',
   database: 'nmea_data',
   port: 5432
-})*/
-
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
-
-/*app.get('/user', (req, res) => {
-  const username = 'jasu' // TODO: Min/max length
-  const password = 'password' // TODO: Min/max length
-  bcrypt
-    .hash(password, 10)
-    .then((hash: string) => db.postUser(username, hash))
-    .then(() => {
-      res.send('Hello!')
-    })
-    .catch((err: Error) => {
-      console.log(err) // TODO: Log?
-    })
-})*/
-
-/*const compare = (password: String, hash: String) => {
-  bcrypt.compare(password, hash)
 }
 
-const hashWithSalt = (password: String) => {
-  bcrypt.hash(password, 10)
-}*/
+const dbClient = new Queries(postgresConfig)
+const api = new Endpoints(dbClient)
 
-/*app.post('/nmea', (req, res) => {
-  db.postNMEA()
-  res.send('Hello!')
-})*/
+const app = express()
+const port = 8000
+
+const catchExceptions = (callback: Function) => // eslint-disable-line
+  (req: express.Request, res: express.Response) =>
+    callback(req, res).catch((err: any) => res.send(err)) // eslint-disable-line
+// TODO: Log errors, or what?
+
+// BOATS
+// getOne
+// getAll
+// create
+// delete
+// update
+
+// USERS
+app.get('/users', catchExceptions(api.users.getAll))
+app.get('/users/:id', catchExceptions(api.users.getOne))
+app.post('/users', catchExceptions(api.users.create))
+app.delete('/users/:id', catchExceptions(api.users.delete))
+// update
+
+// NMEA
+// get
+// save
+
+// TODO: app.use(isAuthenticated)
+// TODO: Collect HTTP logs?
 
 app.listen(port, () => {
   console.log(`App started! Listening on port ${port}`)
