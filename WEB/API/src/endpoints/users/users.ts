911bbfb73e7ad2fe9adf58fff48d040e9f869cf7
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-17 01:19:25 +0300

    users API

---
+++ WEB/API/src/endpoints/users/users.ts
@@ -0,0 +1,38 @@
+import express = require('express')
+import bcrypt = require('bcrypt')
+import Queries from '../../queries/queries'
+
+type Request = typeof express.request
+type Response = typeof express.response
+
+export default class Users {
+  database: Queries
+
+  constructor(database: Queries) {
+    this.database = database
+  }
+
+  getOne = (req: Request, res: Response) => {
+    const userId = req.params.id
+    return this.database.users.getOne(userId).then((data) => res.send(data.rows))
+  }
+
+  getAll = (req: Request, res: Response) => this.database.users.getAll().then((data) => res.send(data.rows))
+
+  create = (req: Request, res: Response) => {
+    // TODO: Username/Password min max length
+    const username = 'jasu4'
+    const password = 'pwd'
+    return bcrypt
+      .hash(password, 10)
+      .then((hash: string) => this.database.users.create(username, hash))
+      .then(() => res.status(201).send())
+  }
+
+  delete = (req: Request, res: Response) => {
+    const userId = req.params.id
+    return this.database.users.delete(userId).then(() => res.status(204).send())
+  }
+
+  // update
+}
