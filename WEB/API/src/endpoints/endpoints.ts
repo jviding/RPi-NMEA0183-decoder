865067c583e43d3079e1a47554e9e2bc285a7506
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-17 01:21:00 +0300

    API router

--- WEB/API/src/endpoints/endpoints.ts
+++ WEB/API/src/endpoints/endpoints.ts
@@ -1,37 +1,16 @@
-//import bcrypt = require('bcrypt')
+import Queries from '../queries/queries'
+import Boats from './boats/boats'
+import Users from './users/users'
+import Nmea from './nmea-data/nmea-data'
 
-/*
-app.get('/', (req, res) => {
-  res.send('Hello!')
-})
-*/
-/*
-app.post('/user', (req, res) => {
-  const username = 'jasu'
-  const password = 'password' // TODO: Min length
-  bcrypt.hash(password, 10)
-  .then((hash: String) => db.createUser(username, hash))
-  .then((result) => {
-    res.send('Hello!')
-  })
-  .catch((err: Error) => {
-    // TODO: Log?
-  })
-})*/
+export default class Endpoints {
+  boats: Boats
+  users: Users
+  nmea: Nmea
 
-/*const compare = (password: String, hash: String) => {
-  bcrypt.compare(password, hash)
+  constructor(database: Queries) {
+    this.boats = new Boats(database)
+    this.users = new Users(database)
+    this.nmea = new Nmea(database)
+  }
 }
-
-const hashWithSalt = (password: String) => {
-  bcrypt.hash(password, 10)
-}*/
-
-/*app.post('/nmea', (req, res) => {
-  db.postNMEA()
-  res.send('Hello!')
-})*/
-/*
-module.exports = {
-  createUser
-}*/
