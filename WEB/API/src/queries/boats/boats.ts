d5e1e6685ba2da3149d46182b5c92e643f985c97
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-17 06:00:45 +0300

    boat data

--- WEB/API/src/queries/boats/boats.ts
+++ WEB/API/src/queries/boats/boats.ts
@@ -7,9 +7,30 @@ export default class Boats {
     this.database = database
   }
 
-  // getOne
-  // getAll
-  // create
+  getByID = (boatId: string) =>
+    this.database.query({
+      text: 'SELECT id, name, type, user_id FROM boats WHERE id=$1',
+      values: [boatId]
+    })
+
+  getByUser = (userId: string) =>
+    this.database.query({
+      text: 'SELECT id, name, type, user_id FROM boats WHERE user_id=$1',
+      values: [userId]
+    })
+
+  getAll = () =>
+    this.database.query({
+      text: 'SELECT id, name, type, user_id FROM boats',
+      values: []
+    })
+
+  create = (name: string, type: string, userId: number) =>
+    this.database.query({
+      text: 'INSERT INTO boats(name, type, user_id) VALUES($1, $2, $3)',
+      values: [name, type, userId]
+    })
+
   // delete
   // update
 }
