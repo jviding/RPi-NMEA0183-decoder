ad6c887fd9b101fb3aa2f3417086172c2bb3955c
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-19 04:07:50 +0300

    pretty

--- WEB/API/src/endpoints/boats/boats.ts
+++ WEB/API/src/endpoints/boats/boats.ts
@@ -13,21 +13,21 @@ export default class Boats {
 
   getByID = (req: Request, res: Response) => {
     const boatId = req.params.boatId
-    return this.database.boats.getByID(boatId).then((data) => res.send(data.rows))
+    return this.database.boats.getByID(boatId).then(({ rows }) => res.send(rows))
   }
 
   getByUser = (req: Request, res: Response) => {
     const userId = req.params.userId
-    return this.database.boats.getByUser(userId).then((data) => res.send(data.rows))
+    return this.database.boats.getByUser(userId).then(({ rows }) => res.send(rows))
   }
 
-  getAll = (req: Request, res: Response) => this.database.boats.getAll().then((data) => res.send(data.rows))
+  getAll = (req: Request, res: Response) => this.database.boats.getAll().then(({ rows }) => res.send(rows))
 
   create = (req: Request, res: Response) => {
     // TODO: Name/Type min max length
     // TODO: Change to reading user ID from Cookie !!
     const { name, type, userId } = req.body
-    return this.database.boats.create(name, type, userId).then((data) => res.status(201).send(data.rows[0]))
+    return this.database.boats.create(name, type, userId).then(({ rows }) => res.status(201).send(rows[0]))
   }
 
   delete = (req: Request, res: Response) => {
