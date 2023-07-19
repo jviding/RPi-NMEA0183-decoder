d0e45a8b960b2ce1ac80d00895355798801543f0
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-19 03:24:05 +0300

    sessions

--- WEB/DB/init-db.sql
+++ WEB/DB/init-db.sql
@@ -3,8 +3,21 @@ CREATE TABLE users (
   id INT GENERATED ALWAYS AS IDENTITY,
   username VARCHAR(255) NOT NULL UNIQUE,
   password_hash VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL
 );
+
+CREATE TABLE user_sessions (
+  sid varchar NOT NULL COLLATE "default",
+  sess json NOT NULL,
+  expire timestamp(6) NOT NULL,
+  -- Constraints --
+  CONSTRAINT session_pkey
+    PRIMARY KEY(sid)
+      NOT DEFERRABLE INITIALLY IMMEDIATE
+) WITH (OIDS=FALSE);
+
+CREATE INDEX idx_session_expire ON user_sessions (expire);
 
 CREATE TABLE boats (
   PRIMARY KEY(id),
@@ -46,7 +59,4 @@ CREATE TABLE nmea_data (
       ON DELETE CASCADE
 );
 
-CREATE INDEX idx_nmea_data_by_trip_id ON nmea_data (
-  trip_id,
-  timestamp DESC
-);
+CREATE INDEX idx_nmea_data_by_trip_id ON nmea_data (trip_id, timestamp DESC);
