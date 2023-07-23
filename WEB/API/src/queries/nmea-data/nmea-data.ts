e5aa54c49eec24b341de4a80789d9f6eced31bb9
jasu <jasu@jasubox.myguest.virtualbox.org>
2023-07-24 01:38:19 +0300

    note for performance

--- WEB/API/src/queries/nmea-data/nmea-data.ts
+++ WEB/API/src/queries/nmea-data/nmea-data.ts
@@ -13,6 +13,8 @@ export default class NmeaData {
       values: [tripId]
     })
 
+  // TODO: Use node-cache? If consequtive writes exhaust the server
+  // Test database speed with print(time) before and after the storage
   create = (timestamp: string, nmeaType: string, nmeaData: string, tripId: string) =>
     this.database.query({
       text: 'INSERT INTO nmea_data(timestamp, packet_type, packet_data, trip_id) VALUES($1, $2, $3, $4)',
